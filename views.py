7a8e3323e5e65e80125b3220ca273c1dfa3b0094
Alexey Korolkov <master.of.programs@mail.ru>
2020-03-18 16:59:07 +0500

    Unworking web API (To do: implement it with flask-restful)

--- views.py
+++ views.py
@@ -1,15 +1,43 @@
 from app import *
-from flask import jsonify
+from flask import jsonify, request
+
+
+@app.route('/')
+def hw():
+    return "hello world"
 
 
 @app.route('/mine', methods=['GET'])
 def main():
-    return "Mined a new block"
+    last_block = bc.last_block
+    last_proof = last_block['proof']
+    proof = bc.proof_of_work(last_proof)
+    bc.add_transaction(sender="0", recepient=node_uid, amount=1)
+
+    prev_hash = bc.hash(last_block)
+    block = bc.new_block(proof, prev_hash=prev_hash)
+
+    response = {
+        'message': 'A new block forged',
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'prev_hash': block['prev_hash']
+    }
+    return jsonify(response), 201
 
 
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
-    return 'Made a new transaction'
+    values = request.get_json()
+    required = ['sender', 'recepient', 'amount']
+    if not all(k in values for k in required):
+        return "Missing data", 400
+    index = bc.add_transaction(values['sender'], values['recepient'], values['amount'])
+    response = {
+        'message': f'Transaction will be added to the block: {index}'
+    }
+    return jsonify(response), 201
 
 
 def full_chain():
